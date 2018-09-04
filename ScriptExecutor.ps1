371758fccb53392595134e86686b853fade989e2
dennis di martini ronn <dennis.Ronn@bematech.com.br>
2018-09-04 19:28:13 -0300

    Adicionado Janela para colar o Script + Secure String para senha

--- ScriptExecutor.ps1
+++ ScriptExecutor.ps1
@@ -1,10 +1,89 @@
+#Cria função para caixa de texto
+function Read-MultiLineInputBoxDialog([string]$Message, [string]$WindowTitle, [string]$DefaultText)
+{
+    Add-Type -AssemblyName System.Drawing
+    Add-Type -AssemblyName System.Windows.Forms
+ 
+    # Create the Label.
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Size(10,10) 
+    $label.Size = New-Object System.Drawing.Size(280,20)
+    $label.AutoSize = $true
+    $label.Text = $Message
+ 
+    # Create the TextBox used to capture the user's text.
+    $textBox = New-Object System.Windows.Forms.TextBox 
+    $textBox.Location = New-Object System.Drawing.Size(10,40) 
+    $textBox.Size = New-Object System.Drawing.Size(575,200)
+    $textBox.AcceptsReturn = $true
+    $textBox.AcceptsTab = $false
+    $textBox.Multiline = $true
+    $textBox.ScrollBars = 'Both'
+    $textBox.Text = $DefaultText
+ 
+    # Create the OK button.
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Size(415,250)
+    $okButton.Size = New-Object System.Drawing.Size(75,25)
+    $okButton.Text = "OK"
+    $okButton.Add_Click({ $form.Tag = $textBox.Text; $form.Close() })
+ 
+    # Create the Cancel button.
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Size(510,250)
+    $cancelButton.Size = New-Object System.Drawing.Size(75,25)
+    $cancelButton.Text = "Cancel"
+    $cancelButton.Add_Click({ $form.Tag = $null; $form.Close() })
+ 
+    # Create the form.
+    $form = New-Object System.Windows.Forms.Form 
+    $form.Text = $WindowTitle
+    $form.Size = New-Object System.Drawing.Size(610,320)
+    $form.FormBorderStyle = 'FixedSingle'
+    $form.StartPosition = "CenterScreen"
+    $form.AutoSizeMode = 'GrowAndShrink'
+    $form.Topmost = $True
+    $form.AcceptButton = $okButton
+    $form.CancelButton = $cancelButton
+    $form.ShowInTaskbar = $true
+ 
+    # Add all of the controls to the form.
+    $form.Controls.Add($label)
+    $form.Controls.Add($textBox)
+    $form.Controls.Add($okButton)
+    $form.Controls.Add($cancelButton)
+ 
+    # Initialize and show the form.
+    $form.Add_Shown({$form.Activate()})
+    $form.ShowDialog() > $null   # Trash the text of the button that was clicked.
+ 
+    # Return the text that the user entered.
+    return $form.Tag
+}
+
+#Questionamentos sobre a execução
 $AzureSQLServerName = Read-Host "Digite o caminho de acesso ao seu banco de dados (Ex: IP, Nome do HOST, URI)"
 $AzureSQLDatabaseName = Read-Host "Digite o nome do seu banco de dados"
-$userid = Read-Host "Digite seu usuário"
-$password= Read-Host "Digite a senha de acesso"
-$script = Read-Host "Digite seu Script e pressione Enter"
-write-output = $password
+$userid = Read-Host "Digite seu usuario do banco"
+$password= Read-Host "Digite a senha de acesso" -AsSecureString
+
+#Converte Secure String para PlainText
+$Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($password)
+$result = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Ptr)
+[System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($Ptr)
+$password = $result
+#Cria Caixa de texto para colar o script
+$multiLineText = Read-MultiLineInputBoxDialog -Message "Please enter some text. It can be multiple lines" -WindowTitle "Script Window" -DefaultText "Cole seu script aqui"
+    if ($multiLineText -eq $null) { Write-Host "You clicked Cancel" }
+else { Write-Host "You entered the following text: $multiLineText" }
+$script = $multiLineText
+
+#Define ConnectionString
 $connstring = "Server=tcp:$AzureSQLServerName,1433;Initial Catalog=$AzureSQLDatabaseName;Persist Security Info=False;User ID=$userid;Password=$password;"
+
+#Executa o Script
 write-output "Executando o script"
 $SQLOutput = $(Invoke-Sqlcmd -Query $script -Verbose -ConnectionString $connstring )
-write-output $SQLOutput
+
+#Exibe o Resultado
+write-output $SQLOutput |Format-Table
